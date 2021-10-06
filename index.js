a2f88fe3820a319cf3e175666f5a5a5e8875774e
chetandev955 <80346622+chetandev955@users.noreply.github.com>
2021-10-06 13:16:27 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const config = require('config')
 console.log(config);
 
 app.get('/', (req, res) => {
-  res.send('CICD demo app for dish')
+  res.send('CICD demo avinash sir')
 })
 
 app.get('/status', (req, res) => {
